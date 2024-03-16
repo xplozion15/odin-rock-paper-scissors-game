a89b103e03255b75149d1b1f04684dd5e43bf56e
xplozion15 <89824888+xplozion15@users.noreply.github.com>
2024-03-16 13:37:54 +0530

    make a function for get computer choice and make function for play round and write a prompt to get player input

---
+++ script.js
@@ -0,0 +1,73 @@
+
+function getComputerChoice() {
+    let randomNumber = Math.ceil(Math.random() * 3);
+
+    if (randomNumber === 1) {
+        choice = "ROCK";
+    }
+    else if (randomNumber === 2) {
+        choice = "PAPER";
+    }
+    else {
+        choice = "SCISSORS";
+    }
+    return choice;
+
+}
+
+
+
+
+function playRound(playerSelection, computerSelection) {
+
+
+    if (playerSelection === "ROCK" && computerSelection === "ROCK") {
+        console.log("You and Computer both selected rock! its a Tie!");
+    }
+
+    else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+        console.log("You selected ROCK and computer selected paper! Computer won!");
+    }
+
+    else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+        console.log("You selected ROCK and computer selected scissors! You won");
+
+    }
+
+    else if (playerSelection === "PAPER" && computerSelection === "PAPER") {
+        console.log("You and Computer both seleCTED PAPER! ITS A TIE");
+    }
+
+    else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+        console.log("You selected PAPER and computer selected ROCK! YOU won!");
+    }
+
+    else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+        console.log("You selected PAPER and computer selected scissors! COMPUTER won");
+
+    }
+
+    else if (playerSelection === "SCISSORS" && computerSelection === "SCISSORS") {
+        console.log("You and Computer both seleCTED SCISSORS! ITS A TIE");
+    }
+    else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+        console.log("You selected SCISSORS and computer selected ROCK! COMPUTER WON!");
+    }
+    else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+        console.log("You selected SCISSORS and computer selected PAPER! YOU won");
+    }
+
+}
+
+
+
+
+let computerSelection = getComputerChoice();
+let playerSelection = prompt("Pls enter your choice from ROCK,PAPER OR SCISSORS").toUpperCase();
+playRound(playerSelection,computerSelection);
+
+
+
+
+
+
